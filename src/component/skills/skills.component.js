c94703e58abf901d06dd8a70e70a58ab1fef9721
Shreyansh <avishreyansh@gmail.com>
2019-12-19 11:13:00 +0530

    Code completion for work and skills page.

--- src/component/skills/skills.component.js
+++ src/component/skills/skills.component.js
@@ -4,32 +4,48 @@ import { GetPageContentAction } from '../../store/root.action';
 import { apiEndpoints } from '../../config/api.endpoints';
 
 
-function SkillsComponent( props ){
+function SkillsComponent(props) {
 
     const dispatch = useDispatch();
 
-    useEffect( () => {
-        dispatch( GetPageContentAction( apiEndpoints.skills ) );
-    }, [ dispatch ] )
+    useEffect(() => {
+        dispatch(GetPageContentAction(apiEndpoints.skills));
+    }, [dispatch])
 
-    const skillsContent = useSelector( state => state.skills );
+    const skillsContent = useSelector(state => state.skills);
 
-    const getUiTemplate = useCallback( () => {
-        if( !skillsContent ){
+    const getUiTemplate = useCallback(() => {
+        if (!skillsContent) {
             return <p>Loading.....</p>
         }
-
-        console.log( skillsContent );
-        return(
+        return (
             <div className="skillsWrapper">
-                <h1>Hello</h1>
+                {skillsContent.categories.map((category, categoryIndex) => {
+                    return (
+                        <div className="categoryWrapper" key={categoryIndex}>
+                            <h2>{category.title}</h2>
+                            {category.subTitle ? <span>{category.subTitle}</span> : null}
+                            {category.groups.map((group, index) => {
+                                return (
+                                    <div className="groupWrapper" key={index}>
+                                        <ul>
+                                            {group.items.map((item, itemIndex) => {
+                                                return <li key={itemIndex}>{item.viewContent}</li>
+                                            })}
+                                        </ul>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
             </div>
         )
-    })
-    
-    return(
+    }, [skillsContent])
+
+    return (
         <React.Fragment>
-            { getUiTemplate() }
+            {getUiTemplate()}
         </React.Fragment>
     )
 }
--- src/component/work/work.component.js
+++ src/component/work/work.component.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { GetPageContentAction } from '../../store/root.action';
+import { apiEndpoints } from '../../config/api.endpoints';
 
+function WorkComponent(props) {
 
-function WorkComponent( props ){
-    
-    return(
+    const dispatch = useDispatch();
+
+    const workContent = useSelector(state => state.work);
+
+    useEffect(() => {
+        dispatch(GetPageContentAction(apiEndpoints.work))
+    }, [dispatch])
+
+    const getUiTemplate = useCallback(() => {
+        if (!workContent) {
+            return <p>Loading ..............</p>
+        }
+
+        return workContent.map((item, index) => {
+            return (
+                <div key={index} className="workWrapper">
+                    <h2>{item.title}</h2>
+                    <p>{item.description}</p>
+                    <ul>
+                        {item.stack.map((stack, key) => {
+                            return <li key={key}>{stack.viewContent}</li>
+                        })}
+                    </ul>
+                    <div className="linksWrapper">
+                        { item.links.map( ( link, key ) => {
+                        return <a key={ key } href={ link.href } target="_blank">{ link.viewContent }</a>
+                        })}
+                    </div>
+                </div>
+            )
+        })
+    }, [workContent])
+
+    return (
         <React.Fragment>
-            <h1>Hello from Work Component</h1>
+            {getUiTemplate()}
         </React.Fragment>
     )
 }
