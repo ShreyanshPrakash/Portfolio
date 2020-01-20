8479890b9fc80652b58289198df190feedec6318
Shreyansh <avishreyansh@gmail.com>
2020-01-21 00:56:26 +0530

    Improved logo and added animation slide for pwa banner

--- src/reuseableComponents/header/header.component.js
+++ src/reuseableComponents/header/header.component.js
@@ -3,46 +3,46 @@ import { NavLink } from 'react-router-dom';
 
 import './header.style.css';
 
-function HeaderComponent( props ){
+function HeaderComponent(props) {
 
-    const [ navToggle, setNavToggle ] = useState('close');
-    
-    const openNav = useCallback( () => {
-        setNavToggle( "open" );
-        console.log( navToggle );
-    }, [ navToggle ] )
+    const [navToggle, setNavToggle] = useState('close');
 
-    const closeNav = useCallback( () => {
-        setNavToggle( "close" );
-        console.log( navToggle );
-    }, [ navToggle ] )
+    const openNav = useCallback(() => {
+        setNavToggle("open");
+        console.log(navToggle);
+    }, [navToggle])
 
-    return(
+    const closeNav = useCallback(() => {
+        setNavToggle("close");
+        console.log(navToggle);
+    }, [navToggle])
+
+    return (
         <header className="header">
-                    <h1 className="logo">
-                        <NavLink to="/home">
-                            S<sub>P</sub>
-                        </NavLink>
-                    </h1>
-                    <div className="menu" onClick={ openNav }>
-                        <hr></hr>
-                        <hr></hr>
-                        <hr></hr>
-                        {/* <span className={ menuStyle } ></span> */}
-                    </div>
-                    <div className="overlay" id={ navToggle } onClick={ closeNav }>
-                        <button className="closebtn" >&times;</button>
-                        <div className="overlay-content">
-                            <NavLink to="/home">Home</NavLink>
-                            <NavLink to="/portfolio">Portfolio</NavLink>
-                            <NavLink to="/skills">Skills</NavLink>
-                        </div>
-                    </div>
-                </header>
+            <h1 className="logo">
+                <NavLink to="/home">
+                    S<sub>P</sub>
+                </NavLink>
+            </h1>
+            <div className="menu" onClick={openNav}>
+                <hr></hr>
+                <hr></hr>
+                <hr></hr>
+                {/* <span className={ menuStyle } ></span> */}
+            </div>
+            <div className="overlay" id={navToggle} onClick={closeNav}>
+                <button className="closebtn" >&times;</button>
+                <div className="overlay-content">
+                    <NavLink to="/home">Home</NavLink>
+                    <NavLink to="/portfolio">Portfolio</NavLink>
+                    <NavLink to="/skills">Skills</NavLink>
+                </div>
+            </div>
+        </header>
     )
 
 }
 
-export{
+export {
     HeaderComponent,
 }
