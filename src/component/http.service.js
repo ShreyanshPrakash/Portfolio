866f4983c9155dba929cfff13f76e085ad45d046
Shreyansh <avishreyansh@gmail.com>
2020-01-26 11:47:26 +0530

    Added api timeout and animated the errror banner

--- src/component/http.service.js
+++ src/component/http.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { apiEndpoints } from './../config/api.endpoints';
 
 function getData( url ){
-    return axios.get( apiEndpoints.hostname + url, { timeout: 3*60 } );
+    return axios.get( apiEndpoints.hostname + url, { timeout: 30*1000 } );
 }
 
 export {
